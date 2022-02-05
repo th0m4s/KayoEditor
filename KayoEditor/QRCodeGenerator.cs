8ffd8d03ffb74ae91816bc2dbc316c7127be524c
Thomas LEDOS <thomas.ledos92@gmail.com>
2022-02-05 19:10:27 +0100

    Added base of QRCodeGenerator

---
+++ KayoEditor/QRCodeGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace KayoEditor
+{
+    public static class QRCodeGenerator
+    {
+        private static readonly Regex extentedRegex = new Regex(@"^[a-zA-Z0-9$%*+\-./: ]{0,47}$");
+        private static readonly Regex strictRegex = new Regex(@"^[A-Z0-9$%*+\-./: ]{0,47}$");
+
+        public static Regex ExtendedRegex => extentedRegex;
+        public static Regex StrictRegex => strictRegex;
+
+        public static ImagePSI GenerateQRCode(string text)
+        {
+            throw new NotImplementedException("Cannot generate a QR code yet!");
+        }
+    }
+}
