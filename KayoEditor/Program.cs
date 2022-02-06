f3ecbba3b6160e0dcd0cbc8f830358aa2243ae6d
Thomas LEDOS <thomas.ledos92@gmail.com>
2022-02-06 14:20:25 +0100

    Added try/catch to console program

--- KayoEditor/Program.cs
+++ KayoEditor/Program.cs
@@ -13,12 +13,24 @@ namespace KayoEditor
 
         static int AskInt(string question)
         {
-            return int.Parse(AskString(question));
+            try
+            {
+                return int.Parse(AskString(question));
+            } catch(Exception)
+            {
+                return -1;
+            }
         }
 
         static float AskFloat(string question)
         {
-            return float.Parse(AskString(question));
+            try
+            {
+                return float.Parse(AskString(question));
+            } catch(Exception)
+            {
+                return -1;
+            }
         }
 
         static void Main(string[] args)
@@ -38,70 +50,91 @@ namespace KayoEditor
 
                 if(File.Exists(filename))
                 {
-                    ImagePSI image = new ImagePSI(filename);
+                    ImagePSI image;
+
+                    try
+                    {
+                        image = new ImagePSI(filename);
+                    } catch(Exception e)
+                    {
+                        Console.WriteLine("Cannot load image file (" + e.GetType().Name + "): " + e.Message);
+                        Console.WriteLine("Press a key to continue...");
+
+                        Console.ReadKey();
+
+                        continue;
+                    }
+
                     bool changefile = false;
 
                     while(!changefile)
                     {
                         int choice = AskInt("\nSelect a choice:\n 1 - Greyscale\n 2 - Black and white\n 3 - Flip\n 4 - Scale\n 5 - Save\n 6 - Change file\n 7 - Quit");
-                        switch(choice)
+                        try
                         {
-                            case 1:
-                                image = image.Greyscale();
-                                Console.WriteLine("Image set to greyscale");
-                                break;
-                            case 2:
-                                image = image.BlackAndWhite();
-                                Console.WriteLine("Image set to black and white");
-                                break;
-                            case 3:
-                                string mode = AskString("How do you want to flip your image? ('x', 'y' or 'both')");
-                                bool flipped = true;
-                                switch(mode)
-                                {
-                                    case "x":
-                                        image = image.Flip(FlipMode.FlipX);
-                                        break;
-                                    case "y":
-                                        image = image.Flip(FlipMode.FlipY);
-                                        break;
-                                    case "both":
-                                        image = image.Flip(FlipMode.FlipBoth);
-                                        break;
-                                    default:
-                                        flipped = false;
-                                        break;
-                                }
-                                Console.WriteLine(flipped ? "Image flipped" : "Invalid flip mode!");
-                                break;
-                            case 4:
-                                float scale = AskFloat("Scale?");
-                                if(scale > 0)
-                                {
-                                    image = image.Scale(scale);
-                                    Console.WriteLine("Image scaled");
-                                } else
-                                {
-                                    Console.WriteLine("scale must be strictly positive!");
-                                }
-                                break;
-                            case 5:
-                                string newPath = AskString("Where do you want to save the image? (nothing to cancel or 'source' to overwrite the opened image)");
-                                if(newPath.Trim().Length > 0)
-                                {
-                                    if (newPath == "source")
-                                        newPath = filename;
+                            switch (choice)
+                            {
+                                case 1:
+                                    image = image.Greyscale();
+                                    Console.WriteLine("Image set to greyscale");
+                                    break;
+                                case 2:
+                                    image = image.BlackAndWhite();
+                                    Console.WriteLine("Image set to black and white");
+                                    break;
+                                case 3:
+                                    string mode = AskString("How do you want to flip your image? ('x', 'y' or 'both')");
+                                    bool flipped = true;
+                                    switch (mode)
+                                    {
+                                        case "x":
+                                            image = image.Flip(FlipMode.FlipX);
+                                            break;
+                                        case "y":
+                                            image = image.Flip(FlipMode.FlipY);
+                                            break;
+                                        case "both":
+                                            image = image.Flip(FlipMode.FlipBoth);
+                                            break;
+                                        default:
+                                            flipped = false;
+                                            break;
+                                    }
+                                    Console.WriteLine(flipped ? "Image flipped" : "Invalid flip mode!");
+                                    break;
+                                case 4:
+                                    float scale = AskFloat("Scale?");
+                                    if (scale > 0)
+                                    {
+                                        image = image.Scale(scale);
+                                        Console.WriteLine("Image scaled");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("scale must be strictly positive!");
+                                    }
+                                    break;
+                                case 5:
+                                    string newPath = AskString("Where do you want to save the image? (nothing to cancel or 'source' to overwrite the opened image)");
+                                    if (newPath.Trim().Length > 0)
+                                    {
+                                        if (newPath == "source")
+                                            newPath = filename;
 
-                                    image.Save(newPath);
-                                }
-                                break;
-                            case 6:
-                                changefile = true;
-                                break;
-                            case 7:
-                                changefile = true;
-                                quit = true;
-                                break;
+                                        image.Save(newPath);
+                                    }
+                                    break;
+                                case 6:
+                                    changefile = true;
+                                    break;
+                                case 7:
+                                    changefile = true;
+                                    quit = true;
+                                    break;
+                            }
+                        } catch(Exception e)
+                        {
+                            Console.WriteLine("An error occured while applying the transformation (" + e.GetType().Name + "): " + e.Message);
                         }
                     }
                 }
